f5984bfaea864765e0509bc860a515c58d8852f5
Kennedy <Tuks1032@gmail.com>
2024-01-26 20:29:07 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'nft-preview-card-component-main';
+}
